9e30216f8aa78ecf29ecdd9ce29b6680f5ef0c52
KarbitsCode <107671693+KarbitsCode@users.noreply.github.com>
2025-03-15 08:33:44 +0800

    Fix transform-ternary.js

--- src/utils/transform-ternary.js
+++ src/utils/transform-ternary.js
@@ -1,11 +1,10 @@
 // From: https://github.com/OpenGG/babel-plugin-transform-ternary-to-if-else
 const template = require('@babel/template').default(`
-(function(){
-  if (TEST) {
-    return CONSEQUENT;
-  }
+if (TEST) {
+  return CONSEQUENT;
+} else {
   return ALTERNATE;
-})();
+}
 `);
 
 module.exports = function ({ types: t }) {
